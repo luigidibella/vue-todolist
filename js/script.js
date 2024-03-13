4fcc42317400ca76deefab62a370f78c68da39b3
luigidibella <lykos.omega@gmail.com>
2024-03-13 18:52:18 +0100

    [add] bonus 4

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ createApp({
   data(){
     return{
       todoList:[
-        'Iscriversi a X',
-        'Vedere Fallout serie',
-        'Vedere Ted Talk'
+        {
+          text: 'Inserisci un nuovo task',
+          done: false
+        },
+        {
+          text: 'Clicca sui task completati',
+          done: false
+        },
+        {
+          text: 'Elimina i task completati',
+          done: false
+        }
       ],
       newTask: '',
       errorMsg: '',
@@ -16,20 +25,29 @@ createApp({
 
   methods: {
     addTask(){
-      if(this.newTask.length >= 5){
-        this.todoList.unshift(this.newTask);
+      if(this.newTask.length >= 4){
+        this.todoList.unshift({ text: `${this.newTask}`, done: false},);
         this.newTask = '';
         this.errorMsg = '';
         this.isError = '';
       }else{
-        this.errorMsg = 'Attenzione! il testo deve avere almeno 5 caretteri'
+        this.errorMsg = 'Attenzione! il testo deve avere almeno 4 caretteri'
         this.isError = true;
       }
 
     },
 
     eliminaTask(indice){
-      this.todoList.splice(indice, 1);
+      if(this.todoList[indice].done === true){
+        this.todoList.splice(indice, 1);
+        this.errorMsg = '';
+      }else{
+        this.errorMsg = 'Completa il task per poterlo eliminare'
+      }
     }
   },
+
+  mounted() {
+    console.log();
+  }
 }).mount('#app');
